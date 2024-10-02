300e7c7b98e0be1f0670c1705cf39e272968b69a
21aslade <andrewmslade@gmail.com>
2024-10-02 15:50:07 -0600

    check king for check

--- shared/src/main/java/chess/ImmutableBoard.java
+++ shared/src/main/java/chess/ImmutableBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import chess.ChessPiece.PieceType;
+
 import java.util.stream.Stream;
 
 public interface ImmutableBoard {
@@ -9,23 +11,28 @@ public interface ImmutableBoard {
 
     default boolean isTargeted(ChessPosition pos, ChessGame.TeamColor team) {
         var moveHelper = new MoveHelper(this, pos, team);
+
+        var king = moveHelper.kingMoves()
+            .flatMap(m -> Stream.ofNullable(this.getPiece(m.endPosition())))
+            .filter(p -> p.type() == PieceType.KING);
+
         var pawn = moveHelper.pawnMoves()
             .flatMap(m -> Stream.ofNullable(this.getPiece(m.endPosition())))
-            .filter(p -> p.type() == ChessPiece.PieceType.PAWN);
+            .filter(p -> p.type() == PieceType.PAWN);
 
         var rook = moveHelper.rookMoves()
             .flatMap(m -> Stream.ofNullable(this.getPiece(m.endPosition())))
-            .filter(p -> p.type() == ChessPiece.PieceType.ROOK || p.type() == ChessPiece.PieceType.QUEEN);
+            .filter(p -> p.type() == PieceType.ROOK || p.type() == PieceType.QUEEN);
 
         var bishop = moveHelper.bishopMoves()
             .flatMap(m -> Stream.ofNullable(this.getPiece(m.endPosition())))
-            .filter(p -> p.type() == ChessPiece.PieceType.BISHOP || p.type() == ChessPiece.PieceType.QUEEN);
+            .filter(p -> p.type() == PieceType.BISHOP || p.type() == PieceType.QUEEN);
 
         var knight = moveHelper.knightMoves()
             .flatMap(m -> Stream.ofNullable(this.getPiece(m.endPosition())))
-            .filter(p -> p.type() == ChessPiece.PieceType.KNIGHT);
+            .filter(p -> p.type() == PieceType.KNIGHT);
 
-        return Stream.of(pawn, rook, bishop, knight)
+        return Stream.of(king, pawn, rook, bishop, knight)
             .flatMap(i -> i)
             .findAny().isPresent();
     }
