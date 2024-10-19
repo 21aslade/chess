1fa7bbc7872570dbb99e86b4e19236941d824d6f
21aslade <andrewmslade@gmail.com>
2024-10-18 21:43:57 -0600

    implement MemoryDataAccess

---
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -0,0 +1,76 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemoryDataAccess implements DataAccess {
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<Integer, GameData> games = new HashMap<>();
+    private final Map<String, AuthData> auth = new HashMap<>();
+
+    @Override
+    public void putUser(UserData user) {
+        this.users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        return this.users.get(username);
+    }
+
+    @Override
+    public void clearUsers() {
+        this.users.clear();
+    }
+
+    @Override
+    public void putGame(GameData game) {
+        this.games.put(game.gameId(), game);
+    }
+
+    @Override
+    public int gameCount() {
+        return this.games.size();
+    }
+
+    @Override
+    public GameData getGame(int gameId) throws DataAccessException {
+        return this.games.get(gameId);
+    }
+
+    @Override
+    public List<GameData> getGames() {
+        return new ArrayList<>(this.games.values());
+    }
+
+    @Override
+    public void clearGames() {
+        this.games.clear();
+    }
+
+    @Override
+    public void putAuth(AuthData auth) {
+        this.auth.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String token) {
+        return this.auth.get(token);
+    }
+
+    @Override
+    public void deleteAuth(String token) {
+        this.auth.remove(token);
+    }
+
+    @Override
+    public void clearAuth() {
+        this.auth.clear();
+    }
+}
---
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -0,0 +1,115 @@
+package dataaccess;
+
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DataAccessTests {
+    static class Implementations implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+            return Stream.of(new MemoryDataAccess()).map(Arguments::of);
+        }
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(Implementations.class)
+    void emptyGetUser(DataAccess dataAccess) throws DataAccessException {
+        assertNull(dataAccess.getUser("anything"));
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(Implementations.class)
+    void putUser(DataAccess dataAccess) throws DataAccessException {
+        var user = new UserData("beans", "paradox", "em");
+        dataAccess.putUser(user);
+        var result = dataAccess.getUser(user.username());
+        assertEquals(user, result);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(Implementations.class)
+    void clearUsers(DataAccess dataAccess) throws DataAccessException {
+        var user = new UserData("beans", "paradox", "em");
+        dataAccess.putUser(user);
+        dataAccess.clearUsers();
+        assertNull(dataAccess.getUser(user.username()));
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(Implementations.class)
+    void emptyGetGames(DataAccess dataAccess) throws DataAccessException {
+        assertEquals(0, dataAccess.getGames().size());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(Implementations.class)
+    void putGame(DataAccess dataAccess) throws DataAccessException {
+        var game = new GameData(3, "apple", "dumpling", "gang", new ChessGame());
+        dataAccess.putGame(game);
+        var result = dataAccess.getGame(game.gameId());
+        assertEquals(game, result);
+
+        var game2 = new GameData(game.gameId(), "never", "before", "seen", new ChessGame());
+        dataAccess.putGame(game2);
+        var result2 = dataAccess.getGame(game2.gameId());
+        assertEquals(game2, result2);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(Implementations.class)
+    void getGames(DataAccess dataAccess) throws DataAccessException {
+        var game = new GameData(3, "apple", "dumpling", "gang", new ChessGame());
+        dataAccess.putGame(game);
+        var game2 = new GameData(4, "never", "before", "seen", new ChessGame());
+        dataAccess.putGame(game2);
+
+        var expected = List.of(game, game2);
+        var actual = dataAccess.getGames();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(Implementations.class)
+    void clearGames(DataAccess dataAccess) throws DataAccessException {
+        var game = new GameData(3, "apple", "dumpling", "gang", new ChessGame());
+        dataAccess.putGame(game);
+        dataAccess.clearGames();
+        assertNull(dataAccess.getGame(game.gameId()));
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(Implementations.class)
+    void emptyGetAuth(DataAccess dataAccess) throws DataAccessException {
+        assertNull(dataAccess.getAuth("anything"));
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(Implementations.class)
+    void putAuth(DataAccess dataAccess) throws DataAccessException {
+        var auth = new AuthData("oh", "yeah");
+        dataAccess.putAuth(auth);
+        var result = dataAccess.getAuth(auth.authToken());
+        assertEquals(auth, result);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(Implementations.class)
+    void clearAuth(DataAccess dataAccess) throws DataAccessException {
+        var auth = new AuthData("oh", "yeah");
+        dataAccess.putAuth(auth);
+        dataAccess.clearAuth();
+        assertNull(dataAccess.getAuth(auth.authToken()));
+    }
+}
