0c6a7307d510eaa1046ba7a0030d4246a699b6c8
21aslade <andrewmslade@gmail.com>
2024-10-19 19:21:16 -0600

    implement register handler

---
+++ server/src/main/java/server/ResponseException.java
@@ -0,0 +1,18 @@
+package server;
+
+public class ResponseException extends Exception {
+    private final int httpCode;
+
+    public ResponseException(int httpCode, String message) {
+        super(message);
+        this.httpCode = httpCode;
+    }
+
+    public int httpCode() {
+        return this.httpCode;
+    }
+
+    public String toJson() {
+        return "{ \"message\": \"" + this.getMessage() + "\" }";
+    }
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,18 +1,27 @@
 package server;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import model.UserData;
+import service.Service;
+import service.ServiceException;
 import spark.*;
 
 public class Server {
+    private final DataAccess data = new MemoryDataAccess();
+    private final Gson gson = new Gson();
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
-        // Register your endpoints and handle exceptions here.
-
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        Spark.delete("/db", this::clear);
+        Spark.post("/user", this::register);
+        Spark.exception(ResponseException.class, this::exceptionHandler);
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -21,5 +30,56 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private void exceptionHandler(ResponseException ex, Request req, Response res) {
+        res.status(ex.httpCode());
+        res.body(ex.toJson());
+    }
+
+    private <T> T tryFromJson(String json, Class<T> classOfT) throws ResponseException {
+        try {
+            return gson.fromJson(json, classOfT);
+        } catch (JsonSyntaxException e) {
+            throw new ResponseException(400, "Error: bad request");
+        }
+    }
+
+    private static <T> T tryRun(ServerFunction<T> callback) throws ResponseException {
+        try {
+            return callback.run();
+        } catch (DataAccessException e) {
+            throw new ResponseException(500, "Error: " + e.getMessage());
+        } catch (ServiceException e) {
+            throw switch (e.kind()) {
+                case AlreadyExists -> new ResponseException(403, "Error: already taken");
+                case DoesNotExist, NullInput -> new ResponseException(400, "Error: bad request");
+                case Unauthorized -> new ResponseException(401, "Error: unauthorized");
+            };
+        }
+    }
+
+    private Object register(Request req, Response res) throws ResponseException {
+        res.type("application/json");
+        var user = tryFromJson(req.body(), UserData.class);
+        var authData = Server.tryRun(() -> Service.registerUser(user, data));
+
+        res.status(200);
+        return gson.toJson(authData);
+    }
+
+    private Object clear(Request req, Response res) throws ResponseException {
+        res.type("application/json");
+        Server.tryRun(() -> {
+            Service.clear(data);
+            return false; // Dummy return so the interface works :(
+        });
+
+        res.status(200);
+        return "{}";
+    }
+
+    private interface ServerFunction<T> {
+        T run() throws DataAccessException, ServiceException;
     }
 }
