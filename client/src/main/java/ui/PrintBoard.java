e510fedcedb1ba347bd2be7551d4f04fe1bb0a8f
21aslade <andrewmslade@gmail.com>
2024-11-14 13:46:41 -0700

    print out all chess pieces

--- client/src/main/java/ui/PrintBoard.java
+++ client/src/main/java/ui/PrintBoard.java
@@ -4,6 +4,7 @@ import chess.ChessBoard;
 import chess.ChessGame.TeamColor;
 import chess.ChessPiece;
 import chess.ChessPiece.PieceType;
+import chess.ChessPosition;
 
 import java.util.List;
 import java.util.stream.IntStream;
@@ -11,9 +12,13 @@ import java.util.stream.IntStream;
 import static ui.EscapeSequences.*;
 
 public class PrintBoard {
-    private static final String HEADER_BG_COLOR = SET_BG_COLOR_LIGHT_GREY;
+    private static final String HEADER_BG_COLOR = SET_BG_COLOR_DARK_GREY;
+    private static final String HEADER_FG_COLOR = SET_TEXT_COLOR_WHITE;
     private static final String WHITE_BG_COLOR = SET_BG_COLOR_WHITE;
     private static final String BLACK_BG_COLOR = SET_BG_COLOR_BLACK;
+
+    private static final String WHITE_FG_COLOR = SET_TEXT_COLOR_RED;
+    private static final String BLACK_FG_COLOR = SET_TEXT_COLOR_BLUE;
 
     private static final List<String> COLUMN_HEADERS = List.of(" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h ");
     private static final List<String> ROW_HEADERS = List.of(" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ");
@@ -24,7 +29,7 @@ public class PrintBoard {
         result.append(columnHeaders);
 
         var rowIndexes = IntStream.range(1, ChessBoard.BOARD_SIZE + 1);
-        var rows = team == TeamColor.WHITE ? rowIndexes : rowIndexes.map(r -> ChessBoard.BOARD_SIZE - r);
+        var rows = team == TeamColor.WHITE ? rowIndexes : rowIndexes.map(r -> ChessBoard.BOARD_SIZE - r + 1);
         rows.forEach((r) -> result.append(row(board, r, team)));
 
         result.append(columnHeaders);
@@ -34,27 +39,37 @@ public class PrintBoard {
     private static String columnHeaders(TeamColor team) {
         var headers = team == TeamColor.WHITE ? COLUMN_HEADERS : COLUMN_HEADERS.reversed();
 
-        var result = new StringBuilder().append(HEADER_BG_COLOR).append(EMPTY);
+        var result = new StringBuilder().append(HEADER_BG_COLOR).append(HEADER_FG_COLOR).append(EMPTY);
         headers.forEach(result::append);
-        result.append(EMPTY).append(RESET_BG_COLOR).append("\n");
+        result.append(EMPTY).append(RESET_BG_COLOR).append(RESET_TEXT_COLOR).append("\n");
         return result.toString();
     }
 
     private static String row(ChessBoard board, int row, TeamColor team) {
-        var header = HEADER_BG_COLOR + ROW_HEADERS.get(row - 1) + RESET_BG_COLOR;
+        var header = HEADER_BG_COLOR + HEADER_FG_COLOR + ROW_HEADERS.get(row - 1) + RESET_BG_COLOR + RESET_TEXT_COLOR;
         var result = new StringBuilder().append(header);
 
         var colIndexes = IntStream.range(1, ChessBoard.BOARD_SIZE + 1);
-        var cols = team == TeamColor.WHITE ? colIndexes : colIndexes.map(c -> ChessBoard.BOARD_SIZE - c);
-        cols.forEach((r) -> result.append(EMPTY));
+        var cols = team == TeamColor.WHITE ? colIndexes : colIndexes.map(c -> ChessBoard.BOARD_SIZE - c + 1);
+        cols.forEach((col) -> {
+            var bg = (row + col) % 2 == 0 ? WHITE_BG_COLOR : BLACK_BG_COLOR;
+            result.append(bg);
+            var piece = board.getPiece(new ChessPosition(row, col));
+            result.append(piece(piece));
+        });
 
         result.append(header).append("\n");
         return result.toString();
     }
 
-    private static String piece(PieceType piece, TeamColor team) {
-        var white = team == TeamColor.WHITE;
-        return switch (piece) {
+    private static String piece(ChessPiece piece) {
+        if (piece == null) {
+            return EMPTY;
+        }
+
+        var white = piece.pieceColor() == TeamColor.WHITE;
+        var color = white ? WHITE_FG_COLOR : BLACK_FG_COLOR;
+        var text = switch (piece.type()) {
             case KING -> white ? WHITE_KING : BLACK_KING;
             case QUEEN -> white ? WHITE_QUEEN : BLACK_QUEEN;
             case BISHOP -> white ? WHITE_BISHOP : BLACK_BISHOP;
@@ -63,5 +78,7 @@ public class PrintBoard {
             case PAWN -> white ? WHITE_PAWN : BLACK_PAWN;
             case null -> EMPTY;
         };
+
+        return color + text;
     }
 }
